3de7d3748f33b71cf57ea78e64d8dec5a391fdae
Matt Canavan <mattcanavan@users.noreply.github.com>
2020-12-16 15:03:56 -0800

    Methods writtend. Tests written and passing.

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,10 +6,25 @@ module.exports = {
 };
 
 function success(item) {
+  if (item.enhancement < 20 && item.enhancement >= 0) {
+    item.enhancement++;
+  } else {
+    return { ...item };
+  }
   return { ...item };
 }
 
 function fail(item) {
+  if (item.enhancement >= 15) {
+    item.durability -= 10;
+
+    if (item.enhancement > 16) {
+      item.enhancement -= 1;
+    }
+  } else {
+    //if enhancement < 15
+    item.durability -= 5;
+  }
   return { ...item };
 }
 
